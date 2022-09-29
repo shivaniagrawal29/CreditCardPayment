15bef7fa304ab824729ff68337496ffa02d80e69
sajpatil <sajpatil@INLEN8520050757.corp.capgemini.com>
2022-09-29 11:21:48 +0530

    customer commit trial 1

--- src/main/java/com/entity/Customer.java
+++ src/main/java/com/entity/Customer.java
@@ -1,37 +1,58 @@
 package com.entity;
 
-
-
 import java.time.LocalDate;
 
-
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-
+import javax.persistence.OneToMany;
 
 @Entity
-public class Customer {
+public class Customer extends User{
 
-
-
-   private String userId;
+	@Id
+	private long userId;
     private String name;
     private String email;
     private String contactNo;
     private LocalDate dob;
     private Address address;
     
-    @Id
-    @GeneratedValue
-    public String getUserId() {
-        return userId;
-    }
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [userId=" + userId + ", name=" + name + ", email=" + email + ", contactNo=" + contactNo
+				+ ", dob=" + dob + "]";
+	}
+}
