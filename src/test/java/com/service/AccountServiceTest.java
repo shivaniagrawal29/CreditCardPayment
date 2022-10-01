65adca62d959fa92b7a7ccc1f0d1445b5c20e3e3
shkhande <shkhande@INLEN8520024206.corp.capgemini.com>
2022-10-01 10:54:13 +0530

    added test classes

---
+++ src/test/java/com/service/AccountServiceTest.java
@@ -0,0 +1,34 @@
+package com.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccountServiceTest {
+
+	@Test
+	void testAddAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllAccounts() {
+		fail("Not yet implemented");
+	}
+
+}
